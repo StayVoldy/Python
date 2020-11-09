27d58bb8a4df03fa3c818339690b0e397e43f305
Martin Víšek <visek@gvp.cz>
2020-11-09 23:38:00 +0100

    RANDOM NUMBER GENERATOR

--- Krajta.py
+++ Krajta.py
@@ -1,3 +1,18 @@
-print("Hello World")
+import random
+#* Simple RNG
 
-#TEST
+print("         _____                   _                     _   _                    _                   _____                                 _               ")
+print("        | ___ \                 | |                   | \ | |                  | |                 |  __ \                               | |              ")
+print("        | |_/ / __ _  _ __    __| |  ___   _ __ ___   |  \| | _   _  _ __ ___  | |__    ___  _ __  | |  \/  ___  _ __    ___  _ __  __ _ | |_  ___   _ __ ")
+print("        |    / / _` || '_ \  / _` | / _ \ | '_ ` _ \  | . ` || | | || '_ ` _ \ | '_ \  / _ \| '__| | | __  / _ \| '_ \  / _ \| '__|/ _` || __|/ _ \ | '__|")
+print("        | |\ \| (_| || | | || (_| || (_) || | | | | | | |\  || |_| || | | | | || |_) ||  __/| |    | |_\ \|  __/| | | ||  __/| |  | (_| || |_| (_) || |   ")
+print("        \_| \_|\__,_||_| |_| \__,_| \___/ |_| |_| |_| \_| \_/ \__,_||_| |_| |_||_.__/  \___||_|     \____/ \___||_| |_| \___||_|   \__,_| \__|\___/ |_|   ")
+                                                                                                                                                        
+print("\n              Welcome to my RNG")   
+
+minimum = int(input("\n             Please type minimal number: "))
+maximum = int(input("\n             Please type maximal number: "))
+
+random_number = random.randint(minimum, maximum)
+
+print("\n \n             Your awesome random number is:", random_number)

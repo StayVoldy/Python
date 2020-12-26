3a18e3f79e41be116487a92ae5f5085748f3508c
Martin Víšek <visek@gvp.cz>
2020-12-26 18:27:03 +0100

    Code divided between multiple files

---
+++ Modules/Man.py
@@ -0,0 +1,8 @@
+class White_Man:
+    _id = []        #Canvas ID of man
+    field = []      #Canvas ID of actual field 
+    color = "white"
+class Black_Man:
+    _id = []        #Canvas ID of man
+    field = []      #Canvas ID of actual field
+    color = "red"

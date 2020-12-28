d302774bb6940ae600980025d0a60640fc984047
Martin Víšek <visek@gvp.cz>
2020-12-28 19:54:57 +0100

    Added Lady transformation and bugged Lady movement

--- Modules/Man.py
+++ Modules/Man.py
@@ -3,8 +3,10 @@ class White_Man:
     field = []      #Canvas ID of actual field 
     color = "white"
     counter = int
+    lady = []    #Default False      #When the man gets to the last line, he becomes lady #//(just like Matweí)
 class Black_Man:
     _id = []        #Canvas ID of man
     field = []      #Canvas ID of actual field
     color = "red"
     counter = int
+    lady = []    #Default False     #When the man gets to the last line, he becomes lady #//(just like Matweí)
